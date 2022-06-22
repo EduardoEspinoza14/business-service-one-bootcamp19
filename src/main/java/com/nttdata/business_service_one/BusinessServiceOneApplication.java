3b3311d1b41f555d7ea62ae1270f444e0034dac4
EduardoEspinoza14 <eduardoantonio.espinozallontop@everis.nttdata.com>
2022-06-22 15:25:32 -0500

    Initial commit

---
+++ src/main/java/com/nttdata/business_service_one/BusinessServiceOneApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.business_service_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessServiceOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/business_service_one/BusinessServiceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.business_service_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessServiceOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
